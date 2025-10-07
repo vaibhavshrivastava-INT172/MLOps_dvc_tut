2fdb965a07b3c23de1d269b1cc53d8f93c70bcdc
Vaibhav <vaibhav.shrivastava@antiersolutions.com>
2025-10-07 12:53:28 +0530

    V3 data saved

--- mycode.py
+++ mycode.py
@@ -13,6 +13,9 @@ df = pd.DataFrame(data)
 row1 = {'Name': 'David', 'Age': 40, 'City': 'Houston'}
 df.loc[len(df.index)] = row1
 
+row2 = {'Name': 'Eva', 'Age': 28, 'City': 'Phoenix'}
+df.loc[len(df.index)] = row2
+
 file_path = os.path.join('data', 'people.csv')
 df.to_csv(file_path, index=False)
 print(f"File '{file_path}' created successfully.")  
